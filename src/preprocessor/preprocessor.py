ca0a31cd4512e7d9bac894bc8ff95d4aaaa0365e
Harvey Bastidas <harveybc@users.noreply.github.com>
2020-02-20 20:05:36 -0500

    Added Preprocessor class

---
+++ src/preprocessor/preprocessor.py
@@ -0,0 +1,152 @@
+# -*- coding: utf-8 -*-
+"""
+ To run this script uncomment the following lines in the
+[options.entry_points] section in setup.cfg:
+
+    console_scripts =
+         fibonacci = data_trimmer.skeleton:run
+
+Then run `python setup.py install` which will install the command `fibonacci`
+inside your current environment.
+Besides console scripts, the header (i.e. until _logger...) of this file can
+also be used as template for Python modules.
+
+"""
+
+import argparse
+import sys
+import logging
+
+from data_trimmer import __version__
+
+__author__ = "Harvey Bastidas"
+__copyright__ = "Harvey Bastidas"
+__license__ = "mit"
+
+_logger = logging.getLogger(__name__)
+
+class  Preprocessor:
+    """ Component Tests
+    """
+    
+    def __init__(self):
+    """ Component Tests Constructor
+    """
+        self.input_file = ""
+        """ Path of the test dataset
+        """    
+        self.output_file = ""
+        """ Path of the output dataset
+        """    
+        self.input_config = ""
+        """ Path of the input configuration
+        """    
+        self.output_config = ""
+        """ Path of the output configuration
+        """    
+        self.rows_d = 0
+        """ Number of rows of the test dataset
+        """    
+        self.cols_d = 0
+        """ Number of columns of the test dataset
+        """
+        # get the number of rows and columns of the test dataset
+        self.rows_d, self.cols_d = self.get_size_csv(test_file)
+
+    def get_size_csv(self, csv_file):
+        """ Get the number of rows and columns of a test dataset, used in all tests.
+        
+        Args:
+        csv_file (string): Path and filename of a test dataset
+
+        Returns:
+        (int,int): number of rows, number of columns
+        """
+        rows = list( csv.reader(open(csv_file)) )
+        return len(rows), len(rows[0])
+
+def fib(n):
+    """Fibonacci example function
+
+    Args:
+      n (int): integer
+
+    Returns:
+      int: n-th Fibonacci number
+    """
+    assert n > 0
+    a, b = 1, 1
+    for i in range(n-1):
+        a, b = b, a+b
+    return a
+
+
+def parse_args(args):
+    """Parse command line parameters
+
+    Args:
+      args ([str]): command line parameters as list of strings
+
+    Returns:
+      :obj:`argparse.Namespace`: command line parameters namespace
+    """
+    parser = argparse.ArgumentParser(
+        description="Just a Fibonacci demonstration")
+    parser.add_argument(
+        "--version",
+        action="version",
+        version="preprocessor {ver}".format(ver=__version__))
+    parser.add_argument(
+        dest="n",
+        help="n-th Fibonacci number",
+        type=int,
+        metavar="INT")
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        dest="loglevel",
+        help="set loglevel to INFO",
+        action="store_const",
+        const=logging.INFO)
+    parser.add_argument(
+        "-vv",
+        "--very-verbose",
+        dest="loglevel",
+        help="set loglevel to DEBUG",
+        action="store_const",
+        const=logging.DEBUG)
+    return parser.parse_args(args)
+
+
+def setup_logging(loglevel):
+    """Setup basic logging
+
+    Args:
+      loglevel (int): minimum loglevel for emitting messages
+    """
+    logformat = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
+    logging.basicConfig(level=loglevel, stream=sys.stdout,
+                        format=logformat, datefmt="%Y-%m-%d %H:%M:%S")
+
+
+def main(args):
+    """Main entry point allowing external calls
+
+    Args:
+      args ([str]): command line parameter list
+    """
+    args = parse_args(args)
+    setup_logging(args.loglevel)
+    _logger.debug("Starting crazy calculations...")
+    print("The {}-th Fibonacci number is {}".format(args.n, fib(args.n)))
+    _logger.info("Script ends here")
+
+
+def run():
+    """Entry point for console_scripts
+    """
+    main(sys.argv[1:])
+
+
+if __name__ == "__main__":
+    run()
