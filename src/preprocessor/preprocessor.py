83fa0d87328f0bb00af4b22d66368135a939fae4
Harvey Bastidas <harveybc@users.noreply.github.com>
2020-02-20 20:33:32 -0500

    updated preprocessor class

--- src/preprocessor/preprocessor.py
+++ src/preprocessor/preprocessor.py
@@ -33,25 +33,15 @@ class  Preprocessor:
     """ Component Tests Constructor
     """
         self.input_file = ""
-        """ Path of the test dataset
-        """    
+        """ Path of the test dataset """    
         self.output_file = ""
-        """ Path of the output dataset
-        """    
+        """ Path of the output dataset """    
         self.input_config = ""
-        """ Path of the input configuration
-        """    
+        """ Path of the input configuration """    
         self.output_config = ""
-        """ Path of the output configuration
-        """    
-        self.rows_d = 0
-        """ Number of rows of the test dataset
-        """    
-        self.cols_d = 0
-        """ Number of columns of the test dataset
-        """
-        # get the number of rows and columns of the test dataset
+        """ Path of the output configuration """    
         self.rows_d, self.cols_d = self.get_size_csv(test_file)
+        """ Number of rows and columns in the test dataset """
 
     def get_size_csv(self, csv_file):
         """ Get the number of rows and columns of a test dataset, used in all tests.
@@ -65,24 +55,30 @@ class  Preprocessor:
         rows = list( csv.reader(open(csv_file)) )
         return len(rows), len(rows[0])
 
-def fib(n):
-    """Fibonacci example function
+    def setup_logging(loglevel):
+        """Setup basic logging
 
-    Args:
-      n (int): integer
+        Args:
+        loglevel (int): minimum loglevel for emitting messages
+        """
+        logformat = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
+        logging.basicConfig(level=loglevel, stream=sys.stdout,
+                            format=logformat, datefmt="%Y-%m-%d %H:%M:%S")
 
-    Returns:
-      int: n-th Fibonacci number
-    """
-    assert n > 0
-    a, b = 1, 1
-    for i in range(n-1):
-        a, b = b, a+b
-    return a
+    def main(args):
+        """Main entry point allowing external calls
 
+        Args:
+        args ([str]): command line parameter list
+        """
+        args = parse_args(args)
+        setup_logging(args.loglevel)
+        _logger.debug("Starting crazy calculations...")
+        print("The {}-th Fibonacci number is {}".format(args.n, fib(args.n)))
+        _logger.info("Script ends here")
 
-def parse_args(args):
-    """Parse command line parameters
+    def parse_args(args):
+        """Parse command line parameters, to be overriden by child classes depending on their command line parameters if they are console scripts.
 
     Args:
       args ([str]): command line parameters as list of strings
@@ -90,63 +86,5 @@ def parse_args(args):
     Returns:
       :obj:`argparse.Namespace`: command line parameters namespace
     """
-    parser = argparse.ArgumentParser(
-        description="Just a Fibonacci demonstration")
-    parser.add_argument(
-        "--version",
-        action="version",
-        version="preprocessor {ver}".format(ver=__version__))
-    parser.add_argument(
-        dest="n",
-        help="n-th Fibonacci number",
-        type=int,
-        metavar="INT")
-    parser.add_argument(
-        "-v",
-        "--verbose",
-        dest="loglevel",
-        help="set loglevel to INFO",
-        action="store_const",
-        const=logging.INFO)
-    parser.add_argument(
-        "-vv",
-        "--very-verbose",
-        dest="loglevel",
-        help="set loglevel to DEBUG",
-        action="store_const",
-        const=logging.DEBUG)
-    return parser.parse_args(args)
+    pass 
 
-
-def setup_logging(loglevel):
-    """Setup basic logging
-
-    Args:
-      loglevel (int): minimum loglevel for emitting messages
-    """
-    logformat = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
-    logging.basicConfig(level=loglevel, stream=sys.stdout,
-                        format=logformat, datefmt="%Y-%m-%d %H:%M:%S")
-
-
-def main(args):
-    """Main entry point allowing external calls
-
-    Args:
-      args ([str]): command line parameter list
-    """
-    args = parse_args(args)
-    setup_logging(args.loglevel)
-    _logger.debug("Starting crazy calculations...")
-    print("The {}-th Fibonacci number is {}".format(args.n, fib(args.n)))
-    _logger.info("Script ends here")
-
-
-def run():
-    """Entry point for console_scripts
-    """
-    main(sys.argv[1:])
-
-
-if __name__ == "__main__":
-    run()
