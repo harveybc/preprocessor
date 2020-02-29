14814bb31ba9e19b692bc3e3aeef55fc65f9d692
Harvey Bastidas <harveybc@users.noreply.github.com>
2020-02-29 15:37:09 -0500

    Update test_data_trimmer.py

--- tests/test_data_trimmer.py
+++ tests/test_data_trimmer.py
@@ -60,7 +60,7 @@ class TestDataTrimmer():
         assert (rows_o + cols_o) == (self.rows_d +
                                      self.cols_d) - (rows_t + cols_t)
 
-    def test_C02T02_trim_columns(self):
+    def atest_C02T02_trim_columns(self):
         """ Trims all the constant columns by using the trim_columns method. Execute trimmer with remove_colums = true. """
         rows_t, cols_t = self.dt.trim_columns()
         # save output to file
