b214cffcdd7069a542ae84372e3f759969736f02
Harvey Bastidas <harveybc@users.noreply.github.com>
2020-03-01 20:34:36 -0500

    corrected path

--- tests/data_trimmer/test_data_trimmer.py
+++ tests/data_trimmer/test_data_trimmer.py
@@ -6,7 +6,7 @@ import csv
 import sys
 import os
 #sys.path.append('..\\src\\')
-from preprocessor.data_trimmer.data_trimmer import DataTrimmer
+from preprocessor.data_trimmer.data_trimmer import DataTrimmer 
 
 __author__ = "Harvey Bastidas"
 __copyright__ = "Harvey Bastidas"
