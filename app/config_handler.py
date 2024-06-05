b2cf46f72844f5aeefaf7e60fb41af2247b2d17e
Harvey Bastidas <harveybc@ingeni-us.com>
2024-06-05 01:26:45 -0500

    Update config_handler.py

--- app/config_handler.py
+++ app/config_handler.py
@@ -22,28 +22,6 @@ default_values = {
     'save_config': CONFIG_SAVE_PATH,
     'load_config': CONFIG_LOAD_PATH,
     'quiet_mode': DEFAULT_QUIET_MODE,
-    'window_size': None,
-    'ema_alpha': None,
-    'remove_rows': None,
-    'remove_columns': None,
-    'max_lag': None,
-    'significance_level': None,
-    'alpha': None,
-    'l1_ratio': None,
-    'model_type': None,
-    'timesteps': None,
-    'features': None,
-    'clean_method': None,
-    'period': None,
-    'outlier_threshold': None,
-    'solve_missing': False,
-    'delete_outliers': False,
-    'interpolate_outliers': False,
-    'delete_nan': False,
-    'interpolate_nan': False,
-    'method': None,
-    'single': 0,
-    'multi': [0],
     'force_date': False,
     'headers': False,
     'remote_log': None,
@@ -51,14 +29,6 @@ default_values = {
     'remote_load_config': None,
     'remote_username': 'test',
     'remote_password': 'pass'
-}
-
-plugin_params = {
-    'normalizer': ['method', 'norm_method', 'range'],
-    'unbiaser': ['method', 'window_size', 'ema_alpha'],
-    'trimmer': ['method', 'remove_rows', 'remove_columns'],
-    'feature_selector': ['method', 'single', 'multi', 'max_lag', 'significance_level'],
-    'cleaner': ['method', 'clean_method', 'period', 'outlier_threshold', 'solve_missing', 'delete_outliers', 'interpolate_outliers', 'delete_nan', 'interpolate_nan']
 }
 
 def load_config(args):
@@ -77,63 +47,16 @@ def load_config(args):
             config.update(remote_config)
             print(f"Downloaded configuration from {args.remote_load_config}")
 
-    config['csv_file'] = args.csv_file if args.csv_file else config.get('csv_file', CSV_INPUT_PATH)
-    config['output_file'] = args.output_file if args.output_file else config.get('output_file', CSV_OUTPUT_PATH)
-    config['plugin_name'] = args.plugin if args.plugin else config.get('plugin_name', DEFAULT_PLUGIN)
-    config['norm_method'] = args.norm_method if args.norm_method else config.get('norm_method', DEFAULT_NORMALIZATION_METHOD)
-    config['range'] = tuple(args.range) if args.range else config.get('range', DEFAULT_NORMALIZATION_RANGE)
-    config['save_config'] = args.save_config if args.save_config else config.get('save_config', CONFIG_SAVE_PATH)
-    config['load_config'] = args.load_config if args.load_config else config.get('load_config', CONFIG_LOAD_PATH)
-    config['quiet_mode'] = args.quiet_mode if args.quiet_mode else config.get('quiet_mode', DEFAULT_QUIET_MODE)
-    config['window_size'] = args.window_size if args.window_size else config.get('window_size')
-    config['ema_alpha'] = args.ema_alpha if args.ema_alpha else config.get('ema_alpha')
-    config['remove_rows'] = args.remove_rows if args.remove_rows else config.get('remove_rows')
-    config['remove_columns'] = args.remove_columns if args.remove_columns else config.get('remove_columns')
-    config['max_lag'] = args.max_lag if args.max_lag else config.get('max_lag')
-    config['significance_level'] = args.significance_level if args.significance_level else config.get('significance_level')
-    config['alpha'] = args.alpha if args.alpha else config.get('alpha')
-    config['l1_ratio'] = args.l1_ratio if args.l1_ratio else config.get('l1_ratio')
-    config['model_type'] = args.model_type if args.model_type else config.get('model_type')
-    config['timesteps'] = args.timesteps if args.timesteps else config.get('timesteps')
-    config['features'] = args.features if args.features else config.get('features')
-    config['clean_method'] = args.clean_method if args.clean_method else config.get('clean_method')
-    config['period'] = args.period if args.period else config.get('period')
-    config['outlier_threshold'] = args.outlier_threshold if args.outlier_threshold else config.get('outlier_threshold')
-    config['solve_missing'] = args.solve_missing if args.solve_missing else config.get('solve_missing', False)
-    config['delete_outliers'] = args.delete_outliers if args.delete_outliers else config.get('delete_outliers', False)
-    config['interpolate_outliers'] = args.interpolate_outliers if args.interpolate_outliers else config.get('interpolate_outliers', False)
-    config['delete_nan'] = args.delete_nan if args.delete_nan else config.get('delete_nan', False)
-    config['interpolate_nan'] = args.interpolate_nan if args.interpolate_nan else config.get('interpolate_nan', False)
-    config['method'] = args.method if args.method else config.get('method')
-    config['single'] = args.single if args.single else config.get('single', 0)
-    config['multi'] = args.multi if args.multi else config.get('multi', [0])
-    config['force_date'] = args.force_date if args.force_date else config.get('force_date', False)
-    config['headers'] = args.headers if args.headers else config.get('headers', False)
-    config['remote_log'] = args.remote_log if args.remote_log else config.get('remote_log')
-    config['remote_save_config'] = args.remote_save_config if args.remote_save_config else config.get('remote_save_config')
-    config['remote_load_config'] = args.remote_load_config if args.remote_load_config else config.get('remote_load_config')
-    config['remote_username'] = args.remote_username if args.remote_username else config.get('remote_username', 'test')
-    config['remote_password'] = args.remote_password if args.remote_password else config.get('remote_password', 'pass')
+    for key in vars(args):
+        if getattr(args, key) is not None:
+            config[key] = getattr(args, key)
 
     return config
 
 def save_config(config):
-    plugin_specific_params = {
-        'normalizer': ['method', 'norm_method', 'range'],
-        'unbiaser': ['method', 'window_size', 'ema_alpha'],
-        'trimmer': ['method', 'remove_rows', 'remove_columns'],
-        'feature_selector': ['method', 'single', 'multi', 'max_lag', 'significance_level'],
-        'cleaner': ['method', 'clean_method', 'period', 'outlier_threshold', 'solve_missing', 'delete_outliers', 'interpolate_outliers', 'delete_nan', 'interpolate_nan'],
-    }
-
-    plugin_name = config['plugin_name'] if config['plugin_name'] != 'default_plugin' else 'normalizer'
-    general_params = ['csv_file', 'plugin_name', 'output_file', 'save_config', 'quiet_mode', 'headers', 'force_date', 'remote_log', 'remote_save_config', 'remote_load_config']
-    selected_plugin_params = plugin_specific_params.get(plugin_name, [])
-    filtered_params = {k: v for k, v in config.items() if (k in general_params or k in selected_plugin_params) and v is not None and v != default_values.get(k)}
-
-    config_filename = config['save_config'] if config['save_config'] else 'config_output.json'
+    config_filename = config.get('save_config', 'config_output.json')
     with open(config_filename, 'w') as f:
-        config_str = json.dumps(filtered_params, indent=4)
+        config_str = json.dumps(config, indent=4)
         f.write(config_str)
     return config_str, config_filename
 
