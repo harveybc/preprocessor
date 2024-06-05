6f9bd205d04efa4d1148f66c40b40291bc0d4251
Harvey Bastidas <harveybc@ingeni-us.com>
2024-06-05 02:19:54 -0500

    Update config_handler.py

--- app/config_handler.py
+++ app/config_handler.py
@@ -16,34 +16,10 @@ from app.config import (
 default_values = {
     'csv_file': CSV_INPUT_PATH,
     'output_file': CSV_OUTPUT_PATH,
-    'plugin': DEFAULT_PLUGIN,
-    'norm_method': DEFAULT_NORMALIZATION_METHOD,
-    'range': DEFAULT_NORMALIZATION_RANGE,
+    'plugin_name': DEFAULT_PLUGIN,
     'save_config': CONFIG_SAVE_PATH,
     'load_config': CONFIG_LOAD_PATH,
     'quiet_mode': DEFAULT_QUIET_MODE,
-    'window_size': None,
-    'ema_alpha': None,
-    'remove_rows': None,
-    'remove_columns': None,
-    'max_lag': None,
-    'significance_level': None,
-    'alpha': None,
-    'l1_ratio': None,
-    'model_type': None,
-    'timesteps': None,
-    'features': None,
-    'clean_method': None,
-    'period': None,
-    'outlier_threshold': None,
-    'solve_missing': False,
-    'delete_outliers': False,
-    'interpolate_outliers': False,
-    'delete_nan': False,
-    'interpolate_nan': False,
-    'method': None,
-    'single': 0,
-    'multi': [0],
     'force_date': False,
     'headers': False,
     'remote_log': None,
@@ -53,12 +29,14 @@ default_values = {
     'remote_password': 'pass'
 }
 
-def load_config(args, required_params):
+def load_config(args):
     config = default_values.copy()
+    
     if args.load_config:
         try:
             with open(args.load_config, 'r') as f:
-                config.update(json.load(f))
+                file_config = json.load(f)
+            config.update(file_config)
         except FileNotFoundError:
             print(f"Error: The file {args.load_config} does not exist.")
             raise
@@ -69,33 +47,21 @@ def load_config(args, required_params):
             config.update(remote_config)
             print(f"Downloaded configuration from {args.remote_load_config}")
 
-    for param in required_params:
-        if hasattr(args, param):
-            config[param] = getattr(args, param, default_values.get(param))
-            print(f"Loaded parameter {param} from args with value: {config[param]}")
+    cli_args = vars(args)
+    for key, value in cli_args.items():
+        if value is not None:
+            config[key] = value
 
-    print(f"Final config after merging: {config}")
     return config
 
 def save_config(config):
-    plugin_specific_params = {
-        'normalizer': ['method', 'norm_method', 'range'],
-        'unbiaser': ['method', 'window_size', 'ema_alpha'],
-        'trimmer': ['method', 'remove_rows', 'remove_columns'],
-        'feature_selector': ['method', 'single', 'multi', 'max_lag', 'significance_level'],
-        'cleaner': ['method', 'clean_method', 'period', 'outlier_threshold', 'solve_missing', 'delete_outliers', 'interpolate_outliers', 'delete_nan', 'interpolate_nan'],
-    }
-
-    plugin_name = config['plugin'] if config['plugin'] != 'default_plugin' else 'normalizer'
-    general_params = ['csv_file', 'plugin', 'output_file', 'save_config', 'quiet_mode', 'headers', 'force_date', 'remote_log', 'remote_save_config', 'remote_load_config']
-    selected_plugin_params = plugin_specific_params.get(plugin_name, [])
-    filtered_params = {k: v for k, v in config.items() if (k in general_params or k in selected_plugin_params) and v is not None and v != default_values.get(k)}
+    general_params = ['csv_file', 'plugin_name', 'output_file', 'save_config', 'quiet_mode', 'headers', 'force_date', 'remote_log', 'remote_save_config', 'remote_load_config']
+    filtered_params = {k: v for k, v in config.items() if k in general_params and v is not None and v != default_values.get(k)}
 
     config_filename = config['save_config'] if config['save_config'] else 'config_output.json'
     with open(config_filename, 'w') as f:
         config_str = json.dumps(filtered_params, indent=4)
         f.write(config_str)
-    print(f"Saved config to {config_filename} with content: {config_str}")
     return config_str, config_filename
 
 def load_remote_config(remote_config_url, username, password):
@@ -104,7 +70,6 @@ def load_remote_config(remote_config_url, username, password):
         response.raise_for_status()
         remote_config = response.json()
         if 'json_config' in remote_config:
-            print(f"Loaded remote config: {remote_config['json_config']}")
             return json.loads(remote_config['json_config']), True
         else:
             print("Error: 'json_config' not found in the response.", file=sys.stderr)
@@ -116,4 +81,3 @@ def load_remote_config(remote_config_url, username, password):
 def save_debug_info(debug_info, debug_file):
     with open(debug_file, 'w') as f:
         json.dump(debug_info, f, indent=4)
-    print(f"Saved debug info to {debug_file}")
