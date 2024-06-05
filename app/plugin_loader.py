f1bc12696af4b878664fbeeba900f6927110f35b
Harvey Bastidas <harveybc@ingeni-us.com>
2024-06-05 01:57:13 -0500

    Update plugin_loader.py

--- app/plugin_loader.py
+++ app/plugin_loader.py
@@ -13,7 +13,7 @@ def load_plugin(plugin_name):
     try:
         entry_point = next(pkg_resources.iter_entry_points('preprocessor.plugins', plugin_name))
         plugin = entry_point.load()
-        required_params = getattr(plugin, 'plugin_params', [])
+        required_params = list(plugin.plugin_params.keys())
         return plugin(), required_params
     except StopIteration:
         print(f"Plugin {plugin_name} not found.", file=sys.stderr)
