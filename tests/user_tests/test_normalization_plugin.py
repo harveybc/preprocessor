4c91226a387c8b47090f40d065d7630dc22b428b
Harvey Bastidas <harveybc@ingeni-us.com>
2024-06-06 04:33:30 -0500

    Update test_normalization_plugin.py

--- tests/user_tests/test_normalization_plugin.py
+++ tests/user_tests/test_normalization_plugin.py
@@ -28,8 +28,8 @@ def test_normalization_plugin():
     # Assertions for the debug file
     assert debug_info['input_rows'] == 73841
     assert debug_info['output_rows'] == 73841
-    assert debug_info['input_columns'] == 1
-    assert debug_info['output_columns'] == 1
+    assert debug_info['input_columns'] == 5
+    assert debug_info['output_columns'] == 5
     assert 'execution_time' in debug_info
     assert debug_info['min_val'] is not None
     assert debug_info['max_val'] is not None
