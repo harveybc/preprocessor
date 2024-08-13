be393955a65dc87796fdedd40a9ff337806c4d6d
Harvey Bastidas <harveybc@ingeni-us.com>
2024-08-13 11:45:04 -0500

    Delete post.bat

--- post.bat
+++
@@ -1,3 +0,0 @@
-@echo This script copies the preprocessed dataset with the default plugin to their final locations in ..\feature-extractor, ..\rl-optimizer and ..\Documents (those dirs must exist)
-@echo off
-copy 
