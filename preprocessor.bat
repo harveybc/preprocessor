f8358e0f2d4949fc64d234c68e77c3b45ae9fc34
Harvey Bastidas <harveybc@ingeni-us.com>
2024-12-17 22:43:35 -0500

    Update preprocessor.bat

--- preprocessor.bat
+++ preprocessor.bat
@@ -1,6 +1,6 @@
 @echo off
 set PREV_PYTHONPATH = %PYTHONPATH%
-set PYTHONPATH=%PYTHONPATH%%cd%;
+set PYTHONPATH=%cd%;%PYTHONPATH%
 echo %PYTHONPATH%
 python app/main.py %*
 set PYTHONPATH=%PREV_PYTHONPATH%
